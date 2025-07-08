8dd36af89111218c9360b6dc1e930e60f9b993ef
Joo Lee <joo.lee08@qmail.cuny.edu>
2025-07-07 21:01:11 -0400

    Add event listener that colors in boxes as they are hovered over

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ for (let i = 0; i < 16; i++) {
   const colDiv = document.createElement("div");
   colDiv.classList.add("col");
   container.appendChild(colDiv);
-  
-  for(let j = 0; j < 16; j++) {
+
+  for (let j = 0; j < 16; j++) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
     colDiv.appendChild(rowDiv);
   }
 }
+
+const boxes = document.querySelectorAll(".row");
+boxes.forEach((box) => {
+  box.onmouseenter = () => box.style.background = "black";
+});
+
