1ca2ee478667abbf1fd9d6e61939c18a4f77aace
Joo Lee <joo.lee08@qmail.cuny.edu>
2025-07-07 21:57:43 -0400

    Fix grid container not filling entire screen

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const container = document.querySelector(".container");
-
 for (let i = 0; i < 16; i++) {
   const colDiv = document.createElement("div");
   colDiv.classList.add("col");
@@ -14,6 +13,16 @@ for (let i = 0; i < 16; i++) {
 
 const boxes = document.querySelectorAll(".row");
 boxes.forEach((box) => {
-  box.onmouseenter = () => box.style.background = "black";
+  box.onmouseenter = () => (box.style.background = "black");
 });
 
+const gridEditButton = document.createElement("button");
+gridEditButton.textContent = "Edit Grid";
+gridEditButton.addEventListener("click", () => {
+  let numSquares = prompt("Number of squares per side (100 max)", "16");
+  if (numSquares != null && numSquares <= 100 && numSquares > 0) {
+    generateGrid(numSquares);
+  }
+});
+
+boxes[0].appendChild(gridEditButton);
