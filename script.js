2375408fe80f96d171b3c15569dc28265a805039
Joo Lee <joo.lee08@qmail.cuny.edu>
2025-07-07 20:34:59 -0400

    Create 16x16 grid using flexbox

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+  const colDiv = document.createElement("div");
+  colDiv.classList.add("col");
+  container.appendChild(colDiv);
+  
+  for(let j = 0; j < 16; j++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
+    colDiv.appendChild(rowDiv);
+  }
+}
